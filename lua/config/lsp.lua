8fd1d57d3a23e2ccc92e0f5e63f778ad4e65a5c6
YuanZishun <YZSlone@outlook.com>
2025-04-24 19:28:33 +0800

    feat: 代码提示优化

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -28,3 +28,17 @@ require("mason-lspconfig").setup_handlers {
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+
+-- ui
+-- 对错误警告的图标
+vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+ -- 在输入模式下也更新提示，设置为 true 也许会影响性能
+ update_in_insert = true,
+})
+local signs = { Error = "󰅙", Info = "󰋼", Hint = "󰌵", Warn = "" }
+for type, icon in pairs(signs) do
+ local hl = "DiagnosticSign" .. type
+ vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
